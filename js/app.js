2a2333a392dc89cd2df36218c7f54674f0602e32
Itzel <itzeljimenez.do@gmail.com>
2017-05-29 17:53:25 +0000

    añadiendo efectos a los slides

--- js/app.js
+++ js/app.js
@@ -4,41 +4,40 @@ $(document).ready(function(){
 	$('#point1').css("background-color", "gray");
 	
 	$('#point1').click(function(){
-		$('.slides').hide();
-		$('#no1').show();
+		$('#no1').fadeIn("slow");
 		$('#point1').css("background-color", "gray");
 	});
 
 	$('#point2').click(function(){
 		$('.slides').hide();
-		$('#no2').show();
+		$('#no2').fadeIn("slow");
 		$('.indicador-item').css('background-color', '#fff');
 		$('#point2').css("background-color", "gray");
 	});
 
 	$('#point3').click(function(){
 		$('.slides').hide();
-		$('#no3').show();
+		$('#no3').fadeIn("slow");
 		$('.indicador-item').css('background-color', '#fff');
 		$('#point3').css("background-color", "gray");
 	});
 
 	$('#point4').click(function(){
 		$('.slides').hide();
-		$('#no4').show();
+		$('#no4').fadeIn("slow");
 		$('.indicador-item').css('background-color', '#fff');
 		$('#point4').css("background-color", "gray");
 	});
 
 	$('#point5').click(function(){
 		$('.slides').hide();
-		$('#no5').show();
+		$('#no5').fadeIn("slow");
 		$('.indicador-item').css('background-color', '#fff');
 		$('#point5').css("background-color", "gray");
 	});
 
 	$('.previous').click(function(){
-		alert("washu")
+		$("li").prev().show();
 	});
 
 	$('.next').click(function(){
