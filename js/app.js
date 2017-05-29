a1db37e4e18a3a56010e821808020c8d5ba500a6
Itzel <itzeljimenez.do@gmail.com>
2017-05-29 17:39:04 +0000

    boton next y previous enlazados, navegación de slides funcionando

--- js/app.js
+++ js/app.js
@@ -1,9 +1,47 @@
-          $(document).ready(function() {
-              $(".carousel").carousel({
-                  fullWidth: true
-              });
-              $('.carousel').carousel();
-              setInterval(function() {
-                  $('.carousel').carousel('next');
-              }, 2000);
-          });
+$(document).ready(function(){
+	$('.slides').hide();
+	$('#no1').show();
+	$('#point1').css("background-color", "gray");
+	
+	$('#point1').click(function(){
+		$('.slides').hide();
+		$('#no1').show();
+		$('#point1').css("background-color", "gray");
+	});
+
+	$('#point2').click(function(){
+		$('.slides').hide();
+		$('#no2').show();
+		$('.indicador-item').css('background-color', '#fff');
+		$('#point2').css("background-color", "gray");
+	});
+
+	$('#point3').click(function(){
+		$('.slides').hide();
+		$('#no3').show();
+		$('.indicador-item').css('background-color', '#fff');
+		$('#point3').css("background-color", "gray");
+	});
+
+	$('#point4').click(function(){
+		$('.slides').hide();
+		$('#no4').show();
+		$('.indicador-item').css('background-color', '#fff');
+		$('#point4').css("background-color", "gray");
+	});
+
+	$('#point5').click(function(){
+		$('.slides').hide();
+		$('#no5').show();
+		$('.indicador-item').css('background-color', '#fff');
+		$('#point5').css("background-color", "gray");
+	});
+
+	$('.previous').click(function(){
+		alert("washu")
+	});
+
+	$('.next').click(function(){
+		alert("washu")
+	});
+});
