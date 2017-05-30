f8d6a841d1c6c354885a09872aac3557a7b0b032
Itzel <itzeljimenez.do@gmail.com>
2017-05-30 12:56:28 +0000

    carousel sencillo funcional completo

--- js/app.js
+++ js/app.js
@@ -1,16 +1,38 @@
+var target=0;
 var cargarPagina = function(){
+	//elementos
 	var botones = document.querySelectorAll(".control");
 	botones.forEach(function(boton){
 		boton.addEventListener("click", cambiarImagen)
 	})
+	var anterior=document.querySelector(".previous");
+	var siguiente=document.querySelector(".next");
+	anterior.addEventListener("click", anteriorImagen);
+	siguiente.addEventListener("click", siguienteImagen);
 }
 
 var cambiarImagen = function(){
-	var target=this.dataset.target;
+    target=parseInt(this.dataset.target);
+	mostrarImagen(target);
+}
+
+var mostrarImagen=function(target){
 	var lastSlide=document.querySelector("div.active")
 	var slide  = document.querySelector("div[data-slide='" + target + "' ]");
 	lastSlide.classList.remove("active");
 	slide.classList.add("active");
 }
 
+var anteriorImagen=function(e){
+	e.preventDefault();
+	target=target-1;
+	target=(target<0)?3:target--;
+	mostrarImagen(target);
+};
+var siguienteImagen=function(e){
+	e.preventDefault();
+	target=target+1;
+	target=(target>3)?0:target++;
+	mostrarImagen(target);
+};
 window.addEventListener("load", cargarPagina);
